051e2db86a73f0fd71f2a132694898640e1eb811
etkhalil <coderkhalil@gmail.com>
2022-02-22 16:38:11 -0500

    add second model for education information

--- src/pages/home/index.js
+++ src/pages/home/index.js
@@ -9,13 +9,18 @@ const ref = createRef()
 
 function App() {
 
-  const [show, setShow] = useState(false);
+  const [Model1, Model1Show] = useState(false);
+  const [Model2, Model2Show] = useState(false);
   const [fname, setfName] = useState("your name!");
   const [phone, setPhone] = useState("888-999-5555");
   const [email, setEmail] = useState("myemail@mail.com");
   const [overview, setOverview] = useState("I am a reserved but ambitious young professional seeking a career that fits my professional skills, personality, and murderous tendencies. My good birth, excellent education and phenomenal mathematical faculty have allowed me to advance the prospects of several criminal enterprises.");
-  const handleClose = () => setShow(false);
-  const handleShow = () => setShow(true);
+
+  const handleModel1Close = () => Model1Show(false);
+  const handleModel1Show = () => Model1Show(true);
+
+  const handleModel2Close = () => Model2Show(false);
+  const handleModel2Show = () => Model2Show(true);
 
   const handleChange = (val, field) => {
     switch (field) {
@@ -48,11 +53,13 @@ function App() {
         <Container>
             {/* <h1 ref={ref}>heello</h1> */}
             <>
-              <Button variant="primary" onClick={handleShow}>
+              <Button variant="primary" onClick={handleModel1Show}>
                 Launch demo modal
               </Button>
-
-              <Modal show={show} onHide={handleClose} animation={false}>
+              {/** 
+               * first Model for collecting personal information!
+               */}
+              <Modal show={Model1} onHide={handleModel1Close} animation={true}>
                 <Modal.Header closeButton>
                   <Modal.Title>Personal Information:</Modal.Title>
                 </Modal.Header>
@@ -84,8 +91,33 @@ function App() {
                   </Form.Group>
                 </Modal.Body>
                 <Modal.Footer>
-                  <Button variant="secondary" onClick={handleClose}>
+                  <Button variant="secondary" onClick={handleModel1Close}>
                     Close
+                  </Button>
+                  <Button variant="primary" onClick={() => {handleModel1Close(); handleModel2Show()}}>
+                    Next
+                  </Button>
+                </Modal.Footer>
+              </Modal>
+              {/** 
+               * Second Model for collecting education information!
+               */}
+              <Modal show={Model2} onHide={handleModel2Close} animation={true}>
+                <Modal.Header closeButton>
+                  <Modal.Title>Education:</Modal.Title>
+                </Modal.Header>
+                <Modal.Body>
+                  <Form.Group className="mb-3" controlId="formBasicName">
+                    <Form.Label>School Name:</Form.Label>
+                    <Form.Control onChange={(e) => handleChange(e, "name")} type="text" placeholder="Oxford University" />
+                    <Form.Text className="text-muted">
+                      Full name as written on your ID.
+                    </Form.Text>
+                  </Form.Group>
+                </Modal.Body>
+                <Modal.Footer>
+                  <Button variant="secondary" onClick={() => {handleModel2Close(); handleModel1Show()}}>
+                    Back
                   </Button>
                   <Button variant="primary" onClick={downloadCV}>
                     Download
