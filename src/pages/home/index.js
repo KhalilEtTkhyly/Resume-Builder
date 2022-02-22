6c259b2b62d5494d9d7c9437f1280f8c7f7dbff8
etkhalil <coderkhalil@gmail.com>
2022-02-22 16:07:22 -0500

    add model to home page

--- src/pages/home/index.js
+++ src/pages/home/index.js
@@ -1,17 +1,30 @@
-import React, { useCallback, createRef, useEffect } from 'react';
+import React, { useCallback, createRef, useEffect, useState } from 'react';
 import Header from '../../sections/header'
-import Main from '../../sections/main'
-import {Container, Row,} from 'react-bootstrap'
+// import Main from '../../sections/main'
 import Template from "../../templates/1";
 import { toPng } from 'html-to-image';
-
+import {Card, Container, Row, Col, Button, Modal, Form} from 'react-bootstrap'
 
 const ref = createRef()
 
-
 function App() {
 
-  useEffect(() => {
+  const [show, setShow] = useState(false);
+  const [fname, setfName] = useState("your name!");
+  const [phone, setPhone] = useState("your name!");
+  const [email, setEmail] = useState("your name!");
+  const handleClose = () => setShow(false);
+  const handleShow = () => setShow(true);
+
+  const handleChange = (val, field) => {
+    switch (field) {
+      case "name": {setfName(val.target.value); break};
+      case "phone": {setPhone(val.target.value); break};
+      case "email": {setEmail(val.target.value); break};
+    }
+  }
+
+  const downloadCV = () => {
     toPng(ref.current, { cacheBust: true, width: 1300, height: 1500})
       .then((dataUrl) => {
         const link = document.createElement('a')
@@ -22,6 +35,9 @@ function App() {
       .catch((err) => {
         console.log(err)
       })
+  }
+
+  useEffect(() => {
   });
 
   return (
@@ -29,7 +45,49 @@ function App() {
         <Header />
         <Container>
             {/* <h1 ref={ref}>heello</h1> */}
-            <Template reference={ref} />
+            <>
+              <Button variant="primary" onClick={handleShow}>
+                Launch demo modal
+              </Button>
+
+              <Modal show={show} onHide={handleClose} animation={false}>
+                <Modal.Header closeButton>
+                  <Modal.Title>Personal Information:</Modal.Title>
+                </Modal.Header>
+                <Modal.Body>
+                  <Form.Group className="mb-3" controlId="formBasicName">
+                    <Form.Label>Full Name:</Form.Label>
+                    <Form.Control onChange={(e) => handleChange(e, "name")} type="text" placeholder="Full Name" />
+                    <Form.Text className="text-muted">
+                      Full name as written on your ID.
+                    </Form.Text>
+                  </Form.Group>
+                  <Form.Group className="mb-3" controlId="formBasicEmail">
+                    <Form.Label>Phone #:</Form.Label>
+                    <Form.Control onChange={(e) => handleChange(e, "phone")} type="text" placeholder="Full Name" />
+                    <Form.Text className="text-muted">
+                      Full name as written on your ID.
+                    </Form.Text>
+                  </Form.Group>
+                  <Form.Group className="mb-3" controlId="formBasicPhone">
+                    <Form.Label>Email Address:</Form.Label>
+                    <Form.Control onChange={(e) => handleChange(e, "email")} type="text" placeholder="Full Name" />
+                    <Form.Text className="text-muted">
+                      Full name as written on your ID.
+                    </Form.Text>
+                  </Form.Group>
+                </Modal.Body>
+                <Modal.Footer>
+                  <Button variant="secondary" onClick={handleClose}>
+                    Close
+                  </Button>
+                  <Button variant="primary" onClick={downloadCV}>
+                    Download
+                  </Button>
+                </Modal.Footer>
+              </Modal>
+            </>
+            <Template fname={fname} phone={phone} email={email} reference={ref} />
         </Container>
     </>
     );
