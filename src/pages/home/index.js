33319a2f5780502fe6f054db1ef8dc3c34c2f04f
etkhalil <coderkhalil@gmail.com>
2022-02-24 23:24:23 -0500

    add features to homepage

--- src/pages/home/index.js
+++ src/pages/home/index.js
@@ -1,16 +1,21 @@
 import React, { createRef, useEffect, useState } from 'react';
-import { Carousel, Button } from 'react-bootstrap';
+import { Carousel, Button, Container, Row, Col, Card } from 'react-bootstrap';
 import Header from '../../sections/header'
 import image from '../../assets/images/resume_1.jpg'
 
+import './home.css';
+
 function App() {
-  
+
   useEffect(() => {
   });
 
   return (
     <>
       <Header />
+      {/** 
+       * Carousel
+      */}
       <Carousel style={{minHeight: "500px", background: "#eee"}}>
         <Carousel.Item style={{minHeight: "500px", backgroundImage: "url("+image+")", backgroundRepeat: "no-repeat", backgroundPosition: "top"}}>
           <Carousel.Caption style={{background: "rgba(0,0,0, .4)"}}>
@@ -29,6 +34,73 @@ function App() {
           </Carousel.Caption>
         </Carousel.Item>
       </Carousel>
+      {/** 
+       * Features
+      */}
+      <Container>
+        <h1 className='h3 my-5 text-center'>Resumes made easy!</h1>
+        <Row>
+          <Col className='text-center'>
+            <div className='circle mb-3'><i className="bi bi-cash"></i></div>
+            <h3 className='h4'>Free of charge</h3>
+            <p>some text here</p>
+          </Col>
+          <Col className='text-center'>
+            <div className='circle mb-3'><i className="bi bi-person-x"></i></div>
+            <h3 className='h4'>No account needed</h3>
+            <p>some text here</p>
+          </Col>
+          <Col className='text-center'>
+            <div className='circle mb-3'><i className="bi bi-stack"></i></div>
+            <h3 className='h4'>multi templates</h3>
+            <p>some text here</p>
+          </Col>
+        </Row>
+      </Container>
+      <Container>
+        <h1 className='h3 my-5 text-center'>Templates</h1>
+        <Row>
+          <Col className='text-center'>
+            <Card style={{ width: '18rem' }}>
+              <Card.Img variant="top" src={image} />
+              <Card.Body>
+                <Card.Title>John Deo</Card.Title>
+                <Card.Text>
+                  Some quick example text to build on the card title and make up the bulk of
+                  the card's content.
+                </Card.Text>
+                <Button variant="primary">Use this!</Button>
+              </Card.Body>
+            </Card>
+          </Col>
+          <Col className='text-center'>
+            <Card style={{ width: '18rem' }}>
+              <Card.Img variant="top" src={image} />
+              <Card.Body>
+                <Card.Title>John Deo</Card.Title>
+                <Card.Text>
+                  Some quick example text to build on the card title and make up the bulk of
+                  the card's content.
+                </Card.Text>
+                <Button variant="primary">Use this!</Button>
+              </Card.Body>
+            </Card>
+          </Col>
+          <Col className='text-center'>
+            <Card style={{ width: '18rem' }}>
+              <Card.Img variant="top" src={image} />
+              <Card.Body>
+                <Card.Title>John Deo</Card.Title>
+                <Card.Text>
+                  Some quick example text to build on the card title and make up the bulk of
+                  the card's content.
+                </Card.Text>
+                <Button variant="primary">Use this!</Button>
+              </Card.Body>
+            </Card>
+          </Col>
+        </Row>
+      </Container>
     </>
     );
 }
