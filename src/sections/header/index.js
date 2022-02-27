ecc15398f008559e9ccf45b260e7ed5cb8278817
etkhalil <coderkhalil@gmail.com>
2022-02-27 15:06:20 -0500

    new logo and colors for the header

--- src/sections/header/index.js
+++ src/sections/header/index.js
@@ -1,24 +1,23 @@
 import {Navbar, Container, Nav, NavDropdown, Button} from 'react-bootstrap'
 import { Link } from "react-router-dom";
 
+import './header.css';
+
 function Header() {
     return (
-        <Navbar collapseOnSelect style={{"paddingTop": 20, "paddingBottom": 20}} expand="lg" bg="dark" variant="dark">
+        <Navbar collapseOnSelect style={{"paddingTop": 20, "paddingBottom": 20}} expand="lg">
         <Container>
-        <Link to="/"><Navbar.Brand>ResumeBuilder</Navbar.Brand></Link>
+        <Link to="/" className='logo'><Navbar.Brand>ResumeBuilder <i className="bi bi-dice-5"></i></Navbar.Brand></Link>
         <Navbar.Toggle aria-controls="responsive-navbar-nav" />
         <Navbar.Collapse id="responsive-navbar-nav">
             <Nav className="me-auto">
-            <NavDropdown title="Templates" id="collasible-nav-dropdown">
-                <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
-                <NavDropdown.Item href="#action/3.2">Another action</NavDropdown.Item>
-                <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
-                <NavDropdown.Divider />
-                <NavDropdown.Item href="#action/3.4">Separated link</NavDropdown.Item>
+            <NavDropdown title="Resumes" id="collasible-nav-dropdown">
+                <NavDropdown.Item><Link to="/collection">With Images</Link></NavDropdown.Item>
+                <NavDropdown.Item><Link to="/collection">Without Images</Link></NavDropdown.Item>
             </NavDropdown>
             </Nav>
             <Nav>
-                <Button variant="primary">Get Started!</Button>{' '}
+                <Button style={{background: '#0b090a', border: 0}} size='lg'>Get Started!</Button>{' '}
             </Nav>
         </Navbar.Collapse>
         </Container>
