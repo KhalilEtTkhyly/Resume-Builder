80728609216b5d452cc3eef24248aebdf1895307
etkhalil <coderkhalil@gmail.com>
2022-03-05 11:50:15 -0500

    update header with new routes

--- src/sections/header/index.js
+++ src/sections/header/index.js
@@ -12,12 +12,14 @@ function Header() {
         <Navbar.Collapse id="responsive-navbar-nav">
             <Nav className="me-auto">
             <NavDropdown as="div" title="Resumes" id="collasible-nav-dropdown">
-                <Link to="/collection"><NavDropdown.Item as="span">With Images</NavDropdown.Item></Link>
-                <Link to="/collection"><NavDropdown.Item as="span">Without Images</NavDropdown.Item></Link>
+                <Link to="/resumes/with-image"><NavDropdown.Item as="span">With Images</NavDropdown.Item></Link>
+                <Link to="/resumes/without-image"><NavDropdown.Item as="span">Without Images</NavDropdown.Item></Link>
             </NavDropdown>
             </Nav>
             <Nav>
-                <Button style={{background: '#0b090a', border: 0}} size='lg'>Get Started!</Button>{' '}
+                <Link to="/resumes/">
+                    <Button style={{background: '#0b090a', border: 0}} size='lg'>Get Started!</Button>{' '}
+                </Link>
             </Nav>
         </Navbar.Collapse>
         </Container>
